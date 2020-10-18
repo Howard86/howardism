5e59a0fd99d7d2e3aec38f6da9001ce311e4e77c
Howard Tai <howardtai86@gmail.com>
2020-10-19 02:14:54 +0800

    :wrench: remove mdx config and add redirect /blog -> /

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-});
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "mdx"],
-});
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
