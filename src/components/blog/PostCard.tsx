6382af6b85cb90b3fa1d82aaf1c09797d87fe3f6
Howard Tai <howardtai86@gmail.com>
2020-10-18 23:46:24 +0800

    :sparkles: add PostCard with description

---
+++ src/components/blog/PostCard.tsx
@@ -0,0 +1,51 @@
+import React, { FC, memo } from "react";
+import { Card, Box, Flex, Heading, Text } from "rebass/styled-components";
+
+import { filterLongContent } from "@/utils/filter";
+import RouteLink from "../common/RouteLink";
+
+interface PostCardProps {
+  title: string;
+  date: string;
+  description: string;
+  tags: string[];
+}
+
+const PostCard: FC<PostCardProps> = ({ title, date, description, tags }) => (
+  <RouteLink href={`/blog/${date}`}>
+    <Card
+      sx={{
+        borderStyle: "solid",
+        borderColor: "eggshellLight",
+        borderRadius: "1rem",
+        borderWidth: "1px",
+        "&:hover": {
+          borderColor: "eggshell",
+        },
+        my: [2, 3],
+      }}
+    >
+      <Box m={[2, 3]}>
+        <Flex justifyContent="space-between" mb={2}>
+          <Heading as="h2">{title}</Heading>
+          <Text sx={{ color: "deepChampagne" }}>{date}</Text>
+        </Flex>
+        {tags.map((tag) => (
+          <Text key={`${date}-${tag}`}>{tag}</Text>
+        ))}
+        <Text
+          as="p"
+          sx={{
+            fontSize: [0, 1, 2],
+            color: "grey",
+            whiteSpace: "break-spaces",
+          }}
+        >
+          {filterLongContent(description)}
+        </Text>
+      </Box>
+    </Card>
+  </RouteLink>
+);
+
+export default memo(PostCard);
