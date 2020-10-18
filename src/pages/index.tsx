538839309209dcb7939199686df502f14fb55888
Howard Tai <howardtai86@gmail.com>
2020-10-18 23:49:44 +0800

    :sparkles: add BlogPage & update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,31 @@
 import React, { FC } from "react";
-import { Box, Flex, Heading } from "rebass/styled-components";
+import { GetStaticProps } from "next";
+import matter from "gray-matter";
+import { readFileSync, readdirSync } from "fs";
+import { join } from "path";
+import { Box, Heading } from "rebass/styled-components";
 
-const HomePage: FC = () => {
-  return (
-    <Flex flexDirection="column">
-      <Box m={2}>
-        <Heading>Blog</Heading>
-        {/* TODO: add Posts */}
-      </Box>
-    </Flex>
-  );
+import { MDX_SOURCE_PATH } from "@/constants/mdx";
+import BlogPage, { BlogPageProps } from "@/components/blog/BlogPage";
+
+const MAX_POSTS_PER_PAGE = 5;
+
+export const getStaticProps: GetStaticProps<BlogPageProps> = async () => {
+  const postsMeta = readdirSync(MDX_SOURCE_PATH)
+    .map((fileName) => {
+      const content = readFileSync(join(MDX_SOURCE_PATH, fileName), "utf8");
+      return matter(content).data as FrontMatter.Meta;
+    })
+    .sort((a, b) => (Date.parse(a.date) < Date.parse(b.date) ? 1 : -1))
+    .slice(0, MAX_POSTS_PER_PAGE);
+  return { props: { postsMeta } };
 };
 
-export default HomePage;
+const Blog: FC<BlogPageProps> = ({ postsMeta }) => (
+  <Box mx={[2, 3]}>
+    <Heading mx={[2, 3]}>Howardism</Heading>
+    <BlogPage postsMeta={postsMeta} />
+  </Box>
+);
+
+export default Blog;
