f2195fd2f4fc367b79c173757f1b72cf8b2bf6ed
Howard Tai <howardtai86@gmail.com>
2020-10-18 23:40:45 +0800

    :pencil2: add custom types

---
+++ src/types/front-matter.d.ts
@@ -0,0 +1,11 @@
+declare namespace FrontMatter {
+  interface Meta {
+    title: string;
+    description: string;
+    date: string;
+    lastPostTitle?: string;
+    lastPostDate?: string;
+    nextPostTitle?: string;
+    nextPostDate?: string;
+  }
+}
