d7d986c6de83cccdc1234c54e643e81ad52d848d
Howard Tai <howard@howardism.dev>
2021-03-06 14:36:11 +0800

    :lipstick: update Layout styling

--- apps/blog/src/containers/Layout.tsx
+++ apps/blog/src/containers/Layout.tsx
@@ -1,18 +1,17 @@
 import React, { FC } from "react";
-import { Container, HStack, Divider } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import { Container, HStack, Divider, Box } from "@chakra-ui/react";
 import SideBar from "@/components/layout/SideBar";
 
-const Layout: FC = ({ children }) => {
-  const route = useRouter();
-
-  return (
-    <Container as={HStack} maxW="120ch">
-      <SideBar path={route.pathname} />
+const Layout: FC = ({ children }) => (
+  <Container as={HStack} maxW="120ch" alignItems="start" my={[8, 12, 16]}>
+    <SideBar />
+    <Box h="70vh">
       <Divider orientation="vertical" />
-      <Container as="main">{children}</Container>
+    </Box>
+    <Container maxW="90ch" as="main">
+      {children}
     </Container>
-  );
-};
+  </Container>
+);
 
 export default Layout;
