33e609b6a60be18bf21e32074b6113a64b167ec9
Howard Tai <howard@howardism.dev>
2021-03-12 00:28:33 +0800

    :lipstick: update markdown styles

--- apps/blog/src/components/markdown/index.ts
+++ apps/blog/src/components/markdown/index.ts
@@ -1,13 +1,16 @@
-import { UnorderedList, Text, OrderedList, ListItem, Divider, Code } from "@chakra-ui/layout";
+import { UnorderedList, OrderedList, ListItem, Divider, Code } from "@chakra-ui/layout";
 import type { MdxRemote } from "next-mdx-remote/types";
 import { H1, H2, H3 } from "./Heading";
+import { BlockQuote, Body } from "./Text";
 import Link from "./Link";
 
+// Reference: https://www.markdownguide.org/basic-syntax/
 const markdown: MdxRemote.Components = {
   h1: H1,
   h2: H2,
   h3: H3,
-  p: Text,
+  p: Body,
+  blockquote: BlockQuote,
   ul: UnorderedList,
   ol: OrderedList,
   li: ListItem,
