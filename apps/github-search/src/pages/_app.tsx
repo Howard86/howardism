3360902d17ea459fc19817aba67e291f4203cf6d
Howard Tai <howard@howardism.dev>
2021-03-28 18:06:27 +0800

    :truck: move common/ThemeProvider into components/ThemeProvider

--- apps/github-search/src/pages/_app.tsx
+++ apps/github-search/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 
-import ThemeProvider from "@/common/ThemeProvider";
+import ThemeProvider from "@/components/ThemeProvider";
 import client from "@/utils/apollo-client";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
