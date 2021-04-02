735e7658443d81db54ff8def5019879ed7317270
Howard Tai <howard@howardism.dev>
2021-04-02 11:53:07 +0800

    :sparkles: update HomePage with user input and hook

--- apps/github-search/src/pages/index.tsx
+++ apps/github-search/src/pages/index.tsx
@@ -1,8 +1,28 @@
-import { Container } from "@chakra-ui/react";
 import React, { FC } from "react";
+import { Button, Input, Stack, VStack } from "@chakra-ui/react";
+import useSearch from "@/hooks/use-search";
 
-const Home: FC = () => {
-  return <Container>Home </Container>;
+const HomePage: FC = () => {
+  const { state, result, onType, onSearch } = useSearch();
+
+  return (
+    <VStack my="auto" spacing={[2, 4]} w="full">
+      <Stack direction={["column", "row"]} align="center">
+        <Input
+          type="text"
+          name="search"
+          aria-label="GitHub Search"
+          placeholder="GitHub username"
+          value={state.username}
+          onChange={onType}
+        />
+        <Button onClick={onSearch} isLoading={result.loading}>
+          Search
+        </Button>
+        {/* TODO: add render result */}
+      </Stack>
+    </VStack>
+  );
 };
 
-export default Home;
+export default HomePage;
--- apps/github-search/test/pages/Home.test.tsx
+++ apps/github-search/test/pages/Home.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Home from "@/pages/index";
+import HomePage from "@/pages/index";
 import { render } from "../test-utils";
 
 describe("HomePage", () => {
   it("should render Home page", () => {
-    render(<Home />, {});
+    render(<HomePage />, {});
   });
 });
