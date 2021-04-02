abce48a11e77eeffbe6df2faecaeef55c3630fed
Howard Tai <howard@howardism.dev>
2021-04-02 17:08:22 +0800

    :white_check_mark: mock window.media

--- apps/github-search/test/pages/Home.test.tsx
+++ apps/github-search/test/pages/Home.test.tsx
@@ -3,6 +3,23 @@ import HomePage from "@/pages/index";
 import { render } from "../test-utils";
 
 describe("HomePage", () => {
+  // Reference: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+
   it("should render Home page", () => {
     render(<HomePage />, {});
   });
