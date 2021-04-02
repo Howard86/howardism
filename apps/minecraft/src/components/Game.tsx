2e9bd35d7bcbd3f44ae2fb27b75f7c2512d74924
Howard Tai <howard@howardism.dev>
2021-04-02 23:30:07 +0800

    :sparkles: add CustomCamera

--- apps/minecraft/src/components/Game.tsx
+++ apps/minecraft/src/components/Game.tsx
@@ -3,11 +3,12 @@ import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Vector3 } from "three";
+import CustomCamera from "./Camera";
 
 const Game: FC = () => {
   return (
     <Canvas>
-      {/* TODO: add <Camera /> */}
+      <CustomCamera fov={50} />
       <Sky sunPosition={new Vector3(100, 10, 100)} />
       <ambientLight intensity={0.3} />
       <pointLight intensity={0.8} position={[100, 100, 100]} castShadow />
