f8134e92a687f6d3b8b7c02b38c3aa0a64a1d262
Howard Tai <howard@howardism.dev>
2021-04-03 02:47:33 +0800

    :sparkles: add Cube and useCubeStore

--- apps/minecraft/src/components/Game.tsx
+++ apps/minecraft/src/components/Game.tsx
@@ -6,17 +6,22 @@ import { Vector3 } from "three";
 import CustomCamera from "./Camera";
 import Ground from "./Ground";
 import Player from "./Player";
+import Cube, { useCubeStore } from "./Cube";
 
 const Game: FC = () => {
+  const cubes = useCubeStore((state) => state.cubes);
+
   return (
     <Canvas shadows gl={{ alpha: false }}>
       <CustomCamera fov={50} />
       <Sky sunPosition={new Vector3(100, 10, 100)} />
       <ambientLight intensity={0.3} />
       <pointLight intensity={0.8} position={[100, 100, 100]} castShadow />
-      <Physics>
+      <Physics gravity={[0, -30, 0]}>
         <Ground />
         <Player />
+        <Cube position={[0, 0.5, -10]} />
+        {cubes.map((cube) => cube)}
       </Physics>
     </Canvas>
   );
