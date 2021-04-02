2c68607d4cba78e2130b6c157e9e13fc4a09c7cd
Howard Tai <howard@howardism.dev>
2021-04-03 01:11:27 +0800

    :wrench: add withTM in next.config.js

--- apps/minecraft/next.config.js
+++ apps/minecraft/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["three"]);
+
+module.exports = withTM({
   reactStrictMode: true,
   future: {
     webpack5: true,
   },
-};
+});
