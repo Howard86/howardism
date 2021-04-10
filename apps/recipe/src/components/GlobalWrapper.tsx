b81a17c0f8b9ac6f401304b07c8185b268ebf69a
Howard Tai <howard@howardism.dev>
2021-04-11 01:42:40 +0800

    :sparkles: add NavBar in Global Wrapper

--- apps/recipe/src/components/GlobalWrapper.tsx
+++ apps/recipe/src/components/GlobalWrapper.tsx
@@ -1,6 +1,15 @@
 import React, { FC } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import NavBar from "./NavBar";
 
-const GlobalWrapper: FC = ({ children }) => <ChakraProvider>{children}</ChakraProvider>;
+const GlobalWrapper: FC = ({ children }) => (
+  <ChakraProvider>
+    <NavBar />
+    {/* TODO: update pt with responsive styles */}
+    <Container maxW="full" px="0" pt="20" bg="blackAlpha.800">
+      {children}
+    </Container>
+  </ChakraProvider>
+);
 
 export default GlobalWrapper;
---
+++ apps/recipe/src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from "react";
+import { Flex, IconButton } from "@chakra-ui/react";
+import { IoMdMenu } from "react-icons/io";
+
+import RouteLink from "./RouteLink";
+
+const NavBar: FC = () => {
+  const onOpen = () => {
+    // TODO: add Drawer here
+    alert("clicked");
+  };
+
+  // TODO: change bg when scrolling
+  return (
+    <Flex
+      as="nav"
+      position="fixed"
+      top="0"
+      w="full"
+      h="20"
+      color="white"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      {/* TODO: add logo with fonts */}
+      <RouteLink href="/" ml="8">
+        Recipe
+      </RouteLink>
+      {/* TODO: update onHover & on onFocus */}
+      <IconButton
+        variant="ghost"
+        colorScheme="white"
+        aria-label="open navigation"
+        mr="8"
+        fontSize="35px"
+        icon={<IoMdMenu />}
+        onClick={onOpen}
+      >
+        Show Menu
+      </IconButton>
+    </Flex>
+  );
+};
+
+export default NavBar;
