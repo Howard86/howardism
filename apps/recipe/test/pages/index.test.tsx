6a855f6f99f4d356574da4689ba9bfe7a3f939be
Howard Tai <howard@howardism.dev>
2021-04-19 22:54:47 +0800

    :recycle: update test

--- apps/recipe/test/pages/index.test.tsx
+++ apps/recipe/test/pages/index.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import HomePage from "@/pages/index";
 import { render } from "../testUtils";
-import Home from "@/pages/index";
 
-describe("Home page", () => {
-  it("matches snapshot", () => {
-    const { asFragment } = render(<Home />, {});
-    expect(asFragment()).toMatchSnapshot();
+describe("HomePage", () => {
+  it("should render", () => {
+    render(<HomePage />, {});
   });
 });
