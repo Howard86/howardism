1dc4e4ff4b88768bdf76a0bbc9eb5f42c1903ba2
Howard Tai <howard@howardism.dev>
2021-05-29 13:54:26 +0800

    :sparkles: set up redux provider, store & hooks

--- apps/recipe/src/components/GlobalWrapper.tsx
+++ apps/recipe/src/components/GlobalWrapper.tsx
@@ -1,17 +1,21 @@
 import React, { FC } from "react";
+import { Provider as ReduxProvider } from "react-redux";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import theme from "@/styles/theme";
+import store from "@/redux/store";
 import AuthProvider from "./AuthProvider";
 import NavBar from "./NavBar";
 
 const GlobalWrapper: FC = ({ children }) => (
   <ChakraProvider theme={theme}>
-    <AuthProvider>
-      <NavBar />
-      <Container maxW="full" minH="100vh" px="0" bg="secondary.50">
-        {children}
-      </Container>
-    </AuthProvider>
+    <ReduxProvider store={store}>
+      <AuthProvider>
+        <NavBar />
+        <Container maxW="full" minH="100vh" px="0" bg="secondary.50">
+          {children}
+        </Container>
+      </AuthProvider>
+    </ReduxProvider>
   </ChakraProvider>
 );
 
