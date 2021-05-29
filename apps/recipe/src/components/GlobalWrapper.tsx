0b2765e1ddd500e457395b8bdb3f3a0bb356792e
Howard Tai <howard@howardism.dev>
2021-05-29 15:40:20 +0800

    :recycle: refactor useAuth with redux and remove AuthContext

--- apps/recipe/src/components/GlobalWrapper.tsx
+++ apps/recipe/src/components/GlobalWrapper.tsx
@@ -3,18 +3,15 @@ import { Provider as ReduxProvider } from "react-redux";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import theme from "@/styles/theme";
 import store from "@/redux/store";
-import AuthProvider from "./AuthProvider";
 import NavBar from "./NavBar";
 
 const GlobalWrapper: FC = ({ children }) => (
   <ChakraProvider theme={theme}>
     <ReduxProvider store={store}>
-      <AuthProvider>
-        <NavBar />
-        <Container maxW="full" minH="100vh" px="0" bg="secondary.50">
-          {children}
-        </Container>
-      </AuthProvider>
+      <NavBar />
+      <Container maxW="full" minH="100vh" px="0" bg="secondary.50">
+        {children}
+      </Container>
     </ReduxProvider>
   </ChakraProvider>
 );
--- apps/recipe/src/hooks/useAuth.tsx
+++ apps/recipe/src/hooks/useAuth.tsx
@@ -1,11 +1,10 @@
-import { useContext, useState } from "react";
 import { useToast } from "@chakra-ui/react";
-import axios from "axios";
-import { AuthContext, DEFAULT_JWT } from "@/components/AuthProvider";
 import type { Account } from "@/types/auth";
+import { authLogin, authLogout } from "@/redux/slices/auth";
+import useAppSelector from "./useAppSelector";
+import useAppDispatch from "./useAppDispatch";
 
 type UseAuth = {
-  jwt: string;
   isLoggedIn: boolean;
   login: (account: Account) => void;
   logout: () => void;
@@ -13,30 +12,30 @@ type UseAuth = {
 
 // TODO: consider manage auth by redux
 const useAuth = (): UseAuth => {
-  const { jwt, setJwt } = useContext(AuthContext);
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const dispatch = useAppDispatch();
+  const { isLoggedIn } = useAppSelector((state) => state.auth);
   const toast = useToast({ duration: 5000, isClosable: true, position: "bottom" });
 
-  const login = (account: Account): void => {
-    axios
-      .post("/api/auth/login", account)
-      .then((response) => {
+  const login = async (account: Account): Promise<void> => {
+    const response = await dispatch(authLogin(account));
+
+    switch (response.meta.requestStatus) {
+      case "fulfilled":
         toast({ status: "success", description: "Log in successfully!" });
-        setJwt(response.data.jwt);
-        setLoggedIn(true);
-      })
-      .catch((error) => {
-        console.error(error);
+        break;
+      case "rejected":
         toast({ status: "error", description: "Incorrect account information" });
-      });
+        break;
+      default:
+        throw new Error("Unknown request status");
+    }
   };
 
   const logout = (): void => {
-    setJwt(DEFAULT_JWT);
-    setLoggedIn(false);
+    dispatch(authLogout());
   };
 
-  return { jwt, isLoggedIn, login, logout };
+  return { isLoggedIn, login, logout };
 };
 
 export default useAuth;
