0b5a7aa28c1142aec66f0b0078400a755ef61d36
Howard Tai <howard@howardism.dev>
2021-05-30 23:05:00 +0800

    :sparkles: add CreateRecipePage

---
+++ apps/recipe/src/pages/create.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import RecipeForm from "@/components/RecipeForm";
+import useAuth from "@/hooks/useAuth";
+import { Container, Heading } from "@chakra-ui/react";
+
+const CreateRecipePage = (): JSX.Element => {
+  const router = useRouter();
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/signin");
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <Container my="8" py="8" px={{ base: 4, md: 10 }} bg="white" borderRadius="lg">
+      <Heading textAlign="center" size="xl" fontWeight="extrabold" my="6">
+        Create your own Recipe!
+      </Heading>
+      <RecipeForm />
+    </Container>
+  );
+};
+
+export default CreateRecipePage;
