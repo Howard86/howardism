7a0342d352455092f8af0c9f9ecbfdeea4c056b4
Howard Tai <howard@howardism.dev>
2021-06-19 22:34:53 +0800

    :bug: fix recipes breaking changes

--- apps/recipe/next.config.js
+++ apps/recipe/next.config.js
@@ -2,9 +2,7 @@ const withTM = require("next-transpile-modules")(["@howardism/login-form"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
