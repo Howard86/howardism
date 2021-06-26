52dd5326c3ae200ba53ddf06af5e02c4509635ba
Howard Tai <howard@howardism.dev>
2021-06-26 13:31:13 +0800

    :sparkles: feat(ui): add MobileDrawer

---
+++ apps/recipe/src/components/layouts/MobileDrawer.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  IconButton,
+  List,
+  ListItem,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useRef } from "react";
+import { HiMenuAlt2 } from "react-icons/hi";
+
+import { MENU_LINK_ITEMS } from "@/constants/menu";
+
+import HorizontalLogo from "../graphics/HorizontalLogo";
+import RouteLink from "../RouteLink";
+
+interface MobileDrawerProps {
+  items?: any;
+}
+
+const MobileDrawer = (props: MobileDrawerProps): JSX.Element => {
+  const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <Box display={{ md: "none" }}>
+      <IconButton
+        aria-label="open-drawer"
+        ref={btnRef}
+        color="primary.600"
+        variant="ghost"
+        onClick={onOpen}
+        fontSize="4xl"
+        icon={<HiMenuAlt2 />}
+      >
+        Open
+      </IconButton>
+      <Drawer
+        size="xs"
+        placement="left"
+        {...props}
+        isOpen={isOpen}
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg="primary.200">
+          <DrawerCloseButton />
+          <DrawerHeader
+            borderBottomWidth="1px"
+            borderBottomStyle="solid"
+            borderBottomColor="gray.200"
+          >
+            <RouteLink href="/">
+              <HorizontalLogo isTransparent />
+            </RouteLink>
+          </DrawerHeader>
+          <DrawerBody my="8">
+            <List spacing="8">
+              {MENU_LINK_ITEMS.map((item) => {
+                // TODO: implement active-link
+                const isCurrentPage = router.pathname === item.url;
+                return (
+                  <ListItem key={item.url} listStyle="none">
+                    <RouteLink
+                      py="1"
+                      px="2.5"
+                      color={isCurrentPage ? "primary.500" : "primary.400"}
+                      fontSize="xl"
+                      whiteSpace="nowrap"
+                      fontWeight={isCurrentPage ? "medium" : "normal"}
+                      transition="0.15s ease-in-out"
+                      _hover={{
+                        color: "primary.500",
+                      }}
+                      _before={{
+                        mt: 4,
+                        display: "inline-block",
+                        content: '""',
+                        position: "absolute",
+                        width: 3,
+                        height: 8,
+                        left: 0,
+                        opacity: isCurrentPage ? 1 : 0,
+                        background: "primary.100",
+                        transform: "translateY(-50%)",
+                      }}
+                      href={item.url}
+                      onClick={onClose}
+                    >
+                      {item.label}
+                    </RouteLink>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default MobileDrawer;
---
+++ apps/recipe/src/constants/menu.ts
@@ -0,0 +1,12 @@
+export interface MenuLinkItem {
+  url: string;
+  label: string;
+}
+
+export const MENU_LINK_ITEMS: MenuLinkItem[] = [
+  {
+    url: "/",
+    label: "Home",
+  },
+  { url: "/signin", label: "Log in" },
+];
