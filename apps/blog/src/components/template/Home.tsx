b7c5d03ec6ca27115be57c3dcef3c55d6c9250c6
Howard Tai <howard@howardism.dev>
2021-07-17 22:09:20 +0800

    :lipstick: style: update HomePage style

--- apps/blog/src/components/template/Home.tsx
+++ apps/blog/src/components/template/Home.tsx
@@ -1,57 +1,64 @@
-import { Box, Container, Heading, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { Image } from "@howardism/components-common";
 import React from "react";
 import { FaGithub, FaRegEnvelope, FaTwitter } from "react-icons/fa";
 
 import profile from "@/../public/profile.jpeg";
 
+import type { PostCardProps } from "../blog/PostCard";
+import PostCard from "../blog/PostCard";
 import SocialIconLink from "../blog/SocialIconLink";
 
-const Home = (): JSX.Element => (
-  <Container
-    textAlign="center"
-    width="610"
-    pt={[50, 70, 100, 120, 150]}
-    px={[6, 2.5]}
-    centerContent
-  >
-    <Box
-      w={[180, 180, 220, 270]}
-      h={[180, 180, 220, 270]}
-      p={[5, 5, 6, 8]}
-      mb={[5, 5, 6, 8]}
-      mx="auto"
-      borderWidth="1px"
-      borderStyle="solid"
-      borderColor="blackAlpha.200"
-      borderRadius="full"
+export interface HomeProps {
+  posts: PostCardProps[];
+}
+
+const Home = ({ posts }: HomeProps): JSX.Element => (
+  <>
+    <Container
+      maxW="container.sm"
+      textAlign="center"
+      pt={[50, 70, 100, 120, 150]}
+      px={[6, 2.5]}
+      centerContent
     >
-      <Image
+      <Box
+        w={[180, 180, 220, 270]}
+        h={[180, 180, 220, 270]}
+        p={[5, 5, 6, 8]}
+        mb={[5, 5, 6, 8]}
+        borderWidth="1px"
+        borderStyle="solid"
+        borderColor="blackAlpha.200"
         borderRadius="full"
-        src={profile}
-        alt="profile"
-        placeholder="blur"
-        layout="responsive"
-        width={270}
-        height={270}
-      />
-    </Box>
-    <Heading as="h1" mb={{ base: 4, lg: 5 }} fontSize={["lg", "xl", "2xl"]} fontWeight="medium">
-      Hey! I’m <Text as="b">Howard</Text>
-    </Heading>
-    <Text mb={{ base: 6, lg: 8 }}>A Lifelong Learner</Text>
-    <Wrap>
-      <WrapItem>
-        <SocialIconLink href="https://twitter.com/howard86_" label="Twitter" icon={FaTwitter} />
-      </WrapItem>
-      <WrapItem>
-        <SocialIconLink href="https://github.com/howard86" label="Github" icon={FaGithub} />
-      </WrapItem>
-      <WrapItem>
-        <SocialIconLink href="mailto:howard@howardism.dev" label="Email" icon={FaRegEnvelope} />
-      </WrapItem>
-    </Wrap>
-  </Container>
+      >
+        <Image borderRadius="full" src={profile} alt="profile" width={270} height={270} priority />
+      </Box>
+      <Heading as="h1" mb={{ base: 4, lg: 5 }} fontSize={["lg", "xl", "2xl"]} fontWeight="medium">
+        Hey! I’m <Text as="b">Howard</Text>
+      </Heading>
+      <Text mb={{ base: 6, lg: 8 }}>
+        A Lifelong Learner. Enthusiastic about technology and how it has revolutionised our daily
+        lives.
+      </Text>
+      <Wrap>
+        <WrapItem>
+          <SocialIconLink href="https://twitter.com/howard86_" label="Twitter" icon={FaTwitter} />
+        </WrapItem>
+        <WrapItem>
+          <SocialIconLink href="https://github.com/howard86" label="Github" icon={FaGithub} />
+        </WrapItem>
+        <WrapItem>
+          <SocialIconLink href="mailto:howard@howardism.dev" label="Email" icon={FaRegEnvelope} />
+        </WrapItem>
+      </Wrap>
+    </Container>
+    <SimpleGrid pt={[30, 30, 50]} columns={[1, 1, 2]} spacing={[10, 12, 16]}>
+      {posts.map((post) => (
+        <PostCard key={post.slug} {...post} />
+      ))}
+    </SimpleGrid>
+  </>
 );
 
 export default Home;
