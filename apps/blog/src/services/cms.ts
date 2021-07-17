3aae2379f16908ee4ffbe8a1abaa5508d6234c38
Howard Tai <howard@howardism.dev>
2021-07-17 20:47:34 +0800

    :sparkles: feat(server): update /blug/[slug] generation via slug instead of id

--- apps/blog/src/services/cms.ts
+++ apps/blog/src/services/cms.ts
@@ -1,21 +1,23 @@
 import config from "@/config";
 import type { BlogPost } from "@/types/blog-post";
 
-export const fetchBlogPosts = async (): Promise<BlogPost[]> => {
+const envCheck = () => {
   if (!config.cmsApiEndpoint) {
     throw new Error("env CMS_API_ENDPOINT not found");
   }
+};
 
-  const response = await fetch(`${config.cmsApiEndpoint}/blog-posts?_sort=id`);
+export const fetchBlogPosts = async (): Promise<BlogPost[]> => {
+  envCheck();
+
+  const response = await fetch(`${config.cmsApiEndpoint}/blog-posts?_sort=id:desc`);
   const posts = await response.json();
 
   return posts;
 };
 
 export const fetchBlogPostById = async (id: string): Promise<BlogPost | null> => {
-  if (!config.cmsApiEndpoint) {
-    throw new Error("env CMS_API_ENDPOINT not found");
-  }
+  envCheck();
 
   const response = await fetch(`${config.cmsApiEndpoint}/blog-posts/${id}`);
   if (response.status !== 200) {
@@ -23,4 +25,17 @@ export const fetchBlogPostById = async (id: string): Promise<BlogPost | null> =>
   }
 
   return response.json();
+};
+
+export const fetchBlogPostBySlug = async (slug: string): Promise<BlogPost | null> => {
+  envCheck();
+
+  const response = await fetch(`${config.cmsApiEndpoint}/blog-posts?slug=${slug}`);
+  if (response.status !== 200) {
+    return null;
+  }
+
+  const results = (await response.json()) as BlogPost[];
+
+  return results.length > 0 ? results[0] : null;
 };
