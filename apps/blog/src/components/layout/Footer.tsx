2c53e607fc2e28aeefd56538ee08104a9a4e1c03
Howard Tai <howard@howardism.dev>
2021-07-17 22:23:13 +0800

    :sparkles: feat: add Footer in Layout

---
+++ apps/blog/src/components/layout/Footer.tsx
@@ -0,0 +1,22 @@
+import { Box, Link } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = (): JSX.Element => (
+  <Box
+    as="footer"
+    px="4"
+    py={[6, 8]}
+    mt={[60, 90, 90, 120]}
+    textAlign="center"
+    borderTopWidth="1px"
+    borderTopStyle="solid"
+    borderTopColor="blackAlpha.200"
+  >
+    Copyright &copy; {new Date().getFullYear()}{" "}
+    <Link href="https://github.com/howard86" isExternal>
+      Howard86
+    </Link>
+  </Box>
+);
+
+export default Footer;
--- apps/blog/src/components/layout/Layout.tsx
+++ apps/blog/src/components/layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { Container } from "@chakra-ui/react";
 import React, { FC } from "react";
 
+import Footer from "./Footer";
+
 const Layout: FC = ({ children }) => (
-  <Container as="main" maxW="container.xl">
-    {children}
-  </Container>
+  <>
+    <Container as="main" maxW="container.xl">
+      {children}
+    </Container>
+    <Footer />
+  </>
 );
 
 export default Layout;
