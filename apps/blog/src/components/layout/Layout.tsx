e9539e0f89e1a5c28429d3b83e783ca6e6c76ed7
Howard Tai <howard@howardism.dev>
2021-07-17 22:09:03 +0800

    :lipstick: style: remove SideBar in Layout

--- apps/blog/src/components/layout/Layout.tsx
+++ apps/blog/src/components/layout/Layout.tsx
@@ -1,17 +1,9 @@
-import { Box, Container, Divider, HStack } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import React, { FC } from "react";
 
-import SideBar from "@/components/layout/SideBar";
-
 const Layout: FC = ({ children }) => (
-  <Container as={HStack} maxW="120ch" alignItems="start" my={[8, 12, 16]} spacing={4}>
-    <SideBar />
-    <Box h="70vh">
-      <Divider orientation="vertical" />
-    </Box>
-    <Container as="main" maxW="full">
-      {children}
-    </Container>
+  <Container as="main" maxW="container.xl">
+    {children}
   </Container>
 );
 
