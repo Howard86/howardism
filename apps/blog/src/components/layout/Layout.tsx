959404cec745fb2d28a26200e6464fddd9fc5bff
Howard Tai <howard@howardism.dev>
2022-10-08 12:36:34 +0800

    :dizzy: refactor: replace ScrollToTop wiht ScrollProgress & click

--- apps/blog/src/components/layout/Layout.tsx
+++ apps/blog/src/components/layout/Layout.tsx
@@ -1,14 +1,15 @@
 import { ChildrenProps } from "react";
 import { Box } from "@chakra-ui/react";
 
+import ScrollProgress from "../animations/ScrollProgress";
+
 import Footer from "./Footer";
 import Header from "./Header";
-import ScrollToTop from "./ScrollToTop";
 
 export default function Layout({ children }: ChildrenProps) {
   return (
     <Box position="relative" width="full" height="auto" float="left" sx={{ clear: "both" }}>
-      <ScrollToTop />
+      <ScrollProgress />
       <Header />
       <main>{children}</main>
       <Footer />
