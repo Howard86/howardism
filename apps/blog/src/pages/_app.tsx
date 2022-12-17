60b04cf25a86f36b70a04b51cac4e8605a3abda7
Howard Tai <howard@howardism.dev>
2022-12-17 12:39:50 +0800

    :fire: chore: remove framer motion lazyLoad features

--- apps/blog/src/pages/_app.tsx
+++ apps/blog/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { LazyMotion } from "framer-motion";
+import { domMax, LazyMotion } from "framer-motion";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 
@@ -10,13 +10,11 @@ import "focus-visible";
 
 import "@/styles/globals.css";
 
-const loadFeatures = () => import("@/utils/motion-features").then((res) => res.default);
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...DEFAULT_SEO} />
-      <LazyMotion features={loadFeatures} strict>
+      <LazyMotion features={domMax} strict>
         <div className="fixed inset-0 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
             <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
