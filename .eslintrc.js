d18b62d542a910bed6476bb4f534e600948f9cd1
Howard Tai <howard@howardism.dev>
2023-03-25 10:11:09 +0000

    :wrench: chore: set up shared eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,15 @@
 /** @type{import('eslint').ESLint.ConfigData} */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "simple-import-sort", "import"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:@next/next/recommended",
-    "prettier",
-  ],
+  extends: ["howardism"],
   parserOptions: {
     sourceType: "module",
     project: "./tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
   },
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-    jest: true,
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.ts", "**/*.spec.tsx"],
-      plugins: ["jest"],
-      extends: ["plugin:jest/all", "plugin:testing-library/react"],
-      rules: {
-        "jest/no-hooks": 0,
-      },
-    },
-  ],
-  rules: {
-    "global-require": 0,
-    "import/extensions": 0,
-    "import/order": 0,
-    // TODO: fix when updating eslint config
-    // Reference: https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md#options
-    "import/no-extraneous-dependencies": 0,
-    "import/no-import-module-exports": 0,
-    "import/no-relative-packages": 0,
-    "import/prefer-default-export": 0,
-    "import/first": 2,
-    "import/newline-after-import": 2,
-    "import/no-duplicates": 2,
-    // TODO: move to app level config
-    "no-underscore-dangle": 0,
-    "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
-    "no-param-reassign": [2, { props: true, ignorePropertyModificationsFor: ["state", "req"] }],
-    "no-restricted-syntax": [2, "ForInStatement", "LabeledStatement", "WithStatement"],
-    "react/jsx-props-no-spreading": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/require-default-props": 0,
-    "simple-import-sort/imports": 2,
-    "simple-import-sort/exports": 2,
-    "@typescript-eslint/no-var-requires": 0,
-  },
   settings: {
-    react: {
-      version: "detect",
+    next: {
+      rootDir: ["apps/*/"],
     },
   },
 }
---
+++ packages/eslint-config-howardism/index.js
@@ -0,0 +1,58 @@
+/** @type{import('eslint').ESLint.ConfigData} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@next/next/recommended",
+    "turbo",
+    "prettier",
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.ts", "**/*.spec.tsx", "**/*.test.ts", "**/*.test.tsx"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/all", "plugin:testing-library/react"],
+      rules: {
+        "jest/no-hooks": 0,
+      },
+    },
+  ],
+  rules: {
+    "import/no-extraneous-dependencies": 0,
+    "import/no-relative-packages": 0,
+    "import/extensions": 0,
+    "import/prefer-default-export": 0,
+    "no-param-reassign": [
+      2,
+      {
+        props: true,
+        ignorePropertyModificationsFor: ["state", "req"],
+      },
+    ],
+    "no-restricted-syntax": [2, "ForInStatement", "LabeledStatement", "WithStatement"],
+    "no-underscore-dangle": 0,
+    "global-require": 0,
+    "react/jsx-key": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/require-default-props": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@next/next/no-html-link-for-pages": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
