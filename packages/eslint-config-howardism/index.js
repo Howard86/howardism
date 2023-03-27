41ecac8607e79cb59af71c36c20ec3822045426e
Howard Tai <howard@howardism.dev>
2023-03-28 06:46:41 +0800

    :sparkles: feat(eslint): add sort plugins and update eslint rules

--- packages/eslint-config-howardism/index.js
+++ packages/eslint-config-howardism/index.js
@@ -1,9 +1,8 @@
-const path = require("node:path")
-
 /** @type{import('eslint').ESLint.ConfigData} */
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  plugins: ["simple-import-sort", "import"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -14,7 +13,7 @@ module.exports = {
     "prettier",
   ],
   parserOptions: {
-    tsconfigRootDir: path.join(__dirname, "../.."),
+    sourceType: "module",
     project: "./tsconfig.eslint.json",
   },
   env: {
@@ -26,7 +25,7 @@ module.exports = {
   overrides: [
     {
       files: ["**/*.spec.ts", "**/*.spec.tsx", "**/*.test.ts", "**/*.test.tsx"],
-      plugins: ["jest"],
+      plugins: ["jest", "testing-library"],
       extends: ["plugin:jest/all", "plugin:testing-library/react"],
       rules: {
         "jest/no-hooks": 0,
@@ -34,9 +33,12 @@ module.exports = {
     },
   ],
   rules: {
-    "import/no-extraneous-dependencies": 0,
-    "import/no-relative-packages": 0,
     "import/extensions": 0,
+    "import/first": 2,
+    "import/newline-after-import": 0,
+    "import/no-duplicates": 2,
+    "import/no-extraneous-dependencies": 0,
+    "import/order": 0,
     "import/prefer-default-export": 0,
     "no-param-reassign": [
       2,
@@ -49,10 +51,12 @@ module.exports = {
     "no-restricted-syntax": [2, "ForInStatement", "LabeledStatement", "WithStatement"],
     "no-underscore-dangle": 0,
     "global-require": 0,
-    "react/jsx-key": 0,
     "react/jsx-props-no-spreading": 0,
     "react/react-in-jsx-scope": 0,
     "react/require-default-props": 0,
+    "simple-import-sort/imports": 2,
+    "simple-import-sort/exports": 2,
+    "sort-imports": 0,
     "@typescript-eslint/no-var-requires": 0,
     "@next/next/no-html-link-for-pages": 0,
   },
