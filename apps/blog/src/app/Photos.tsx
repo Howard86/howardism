f34f272358b4bdfd1c6f4dc4f720713e9b6a397d
Howard Tai <howard@howardism.dev>
2023-05-14 22:20:02 +0800

    :bug: fix: update image css

--- apps/blog/src/app/Photos.tsx
+++ apps/blog/src/app/Photos.tsx
@@ -25,7 +25,7 @@ export default function Photos() {
             <Image
               src={image}
               alt={image.src}
-              className="absolute inset-0 h-auto w-full object-cover"
+              className="absolute inset-0 h-full w-full object-cover"
               sizes="(min-width: 640px) 18rem, 11rem"
             />
           </div>
