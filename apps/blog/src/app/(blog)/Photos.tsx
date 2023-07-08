939870866e360f84fdec1eaae46f4430a719f46a
Howard Tai <howard@howardism.dev>
2023-07-09 06:58:40 +0800

    :recycle: refactor: update home page photos

--- apps/blog/src/app/(blog)/Photos.tsx
+++ apps/blog/src/app/(blog)/Photos.tsx
@@ -1,35 +1,37 @@
 import clsx from "clsx"
 import Image from "next/image"
 
-import image1 from "@/assets/alexandre-debieve-chip.jpg"
-import image2 from "@/assets/carl-heyerdahl-desk.jpg"
-import image3 from "@/assets/john-morgan-sudoku.jpg"
-import image4 from "@/assets/thisisengineering-raeng-desk.jpg"
+import { getArticles } from "./articles/service"
 
-const PHOTOS = [image1, image2, image3, image4]
-
-export default function Photos() {
-  const rotations = ["rotate-2", "-rotate-2", "rotate-2", "rotate-2", "-rotate-2"]
+export default async function Photos() {
+  const articles = await getArticles()
 
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {PHOTOS.map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              "relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-base-100 sm:w-72 sm:rounded-2xl",
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt={image.src}
-              className="absolute inset-0 h-full w-full object-cover"
-              sizes="(min-width: 640px) 18rem, 11rem"
-            />
-          </div>
-        ))}
+        {articles.ids.map((id, index) => {
+          const article = articles.entities[id]
+
+          if (!article) return null
+
+          return (
+            <div
+              key={article.slug}
+              className={clsx(
+                "relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-base-100 sm:w-72 sm:rounded-2xl",
+                index % 2 ? "rotate-2" : "-rotate-2"
+              )}
+            >
+              <Image
+                src={article.meta.image.src}
+                alt={article.meta.image.alt}
+                className="absolute inset-0 h-full w-full object-cover"
+                sizes="(min-width: 640px) 18rem, 11rem"
+                placeholder="blur"
+              />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
