4f61795e4900bb1feb8c3289c851c8f26647afe1
Howard Tai <howard@howardism.dev>
2023-08-15 21:34:36 +0800

    :bug: fix: add preview credentials for vercel preview env

--- apps/blog/src/pages/api/auth/[...nextauth].ts
+++ apps/blog/src/pages/api/auth/[...nextauth].ts
@@ -1,31 +1,42 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
+import { env } from "@/config/env.mjs"
 import prisma from "@/services/prisma"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers:
+    env.VERCEL_ENV === "preview"
+      ? [
+          CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              username: { label: "Username", type: "text", placeholder: "jsmith" },
+              password: { label: "Password", type: "password" },
+            },
+            authorize: () => ({
+              id: "TEST",
+              name: "Howard Tai",
+              email: "howard+test@howardism.dev",
+              image: "https://avatars.githubusercontent.com/u/42728066?v=4",
+            }),
+          }),
+        ]
+      : [
+          GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+          }),
+          GitHubProvider({
+            clientId: env.GITHUB_ID,
+            clientSecret: env.GITHUB_SECRET,
+          }),
+        ],
   session: { strategy: "jwt" },
-}
-
-if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET)
-  authOptions.providers.push(
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  )
-
-if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
-  authOptions.providers.push(
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  )
 }
 
 export default NextAuth(authOptions)
